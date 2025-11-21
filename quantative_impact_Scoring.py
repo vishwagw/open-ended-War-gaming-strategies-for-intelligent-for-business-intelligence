96e70e1e118026912e2a691f0b0a365be330ba46
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-11-21 23:11:27 +0530

    Add files via upload

---
+++ quantative_impact_Scoring.py
@@ -0,0 +1,8 @@
+def calculate_impact(intervention):
+    impacts = {
+        'time_to_market': -0.3,  # 30% faster
+        'market_share': +0.15,   # 15% increase
+        'burn_rate': +0.25,      # 25% higher costs
+        'competitive_response': 0.4  # 40% stronger reaction
+    }
+    return impacts
